e0221215ab52e5b9aa062ae92c8250fe12f62c66
xujhin <827938508@qq.com>
2025-02-08 10:47:32 +0800

    Initial Commit

---
+++ awesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  awesome
+//
+//  Created by dingstock on 2025/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ awesome/awesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  awesomeApp.swift
+//  awesome
+//
+//  Created by dingstock on 2025/2/8.
+//
+
+import SwiftUI
+
+@main
+struct awesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
